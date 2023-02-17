19dcaf99130626102fbb5cb654fccf73f3957f1a
kaspareevald <kaspar@ifb.ee>
2023-02-17 09:50:16 +0200

    I added tests for the list and card functions

--- cypress.config.js
+++ cypress.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
     apiKey: '9be7f1fb4d6df4bd48603505d75108c9',
     apiToken: 'ATTAc2d2170cdeef873276e114363f69b2ab2ad32e8bb1f0bf66b6935f39f5fbb219224BC6A4',
     email: 'kaspar@ifb.ee',
-    password: 'Prestake55',
+    password: 'Parool2023',
   }
 
 

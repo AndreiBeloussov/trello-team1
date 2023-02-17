c6d97d7e7f17c7ae3009b1c6096fac6aeb526c93
kaspareevald <116432960+kaspareevald@users.noreply.github.com>
2023-02-17 10:12:02 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -10,18 +10,12 @@ module.exports = defineConfig({
   chromeWebSecurity: false,
   
   env: {
-<<<<<<< HEAD
-    apiKey: '9be7f1fb4d6df4bd48603505d75108c9',
-    apiToken: 'ATTAc2d2170cdeef873276e114363f69b2ab2ad32e8bb1f0bf66b6935f39f5fbb219224BC6A4',
-    email: 'kaspar@ifb.ee',
-    password: 'Parool2023',
-=======
+
     apiKey: 'Paste your api key here',
     apiToken: 'Paste you token here',
     email: 'teamonecerebhub@gmail.com',
     password: '170123teamone',
->>>>>>> parent of 7ddc996 (Added tests for list and card functions)
-  }
 
+}
 
 });
